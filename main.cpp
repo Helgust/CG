3ea17eae3e3af7319acebb0563206c0f661f4757
helgust <egortutun@gmail.com>
2020-02-28 21:51:50 +0300

    *[Main] Add refraction

--- main.cpp
+++ main.cpp
@@ -22,11 +22,12 @@ const int   height   = 796;
 const float fov      = M_PI/2.;
 
 struct Material {
-    Material(const Vec3f &a, const Vec3f &color, const float &spec) : albedo(a), diffuse_color(color), specular(spec) {}
-    Material() : albedo(1,0,0), diffuse_color(), specular() {}
-    Vec3f albedo;
+    Material(const Vec4f &a, const Vec3f &color, const float &spec, const float &r) : albedo(a), diffuse_color(color), specular(spec), refractive_index(r) {}
+    Material() : albedo(1,0,0,0), diffuse_color(), specular() ,refractive_index(1) {}
+    Vec4f albedo;
     Vec3f diffuse_color;
     float specular;
+    float refractive_index;
 };
 
 struct Light {
@@ -64,6 +65,19 @@ Vec3f reflect(const Vec3f &I, const Vec3f &N) {
     return I - N*2.f*(I*N);
 }
 
+Vec3f refract(const Vec3f &I, const Vec3f &N, const float &refractive_index) { // Snell's law
+    float cosi = - std::max(-1.f, std::min(1.f, I*N));
+    float etai = 1, etat = refractive_index;
+    Vec3f n = N;
+    if (cosi < 0) { // if the ray is inside the object, swap the indices and invert the normal to get the correct result
+        cosi = -cosi;
+        std::swap(etai, etat); n = -N;
+    }
+    float eta = etai / etat;
+    float k = 1 - eta*eta*(1 - cosi*cosi);
+    return k < 0 ? Vec3f(0,0,0) : I*eta + n*(eta * cosi - sqrtf(k));
+}
+
 bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N, Material &material) {
     float spheres_dist = std::numeric_limits<float>::max();
     for (size_t i=0; i < spheres.size(); i++) {
@@ -91,27 +105,39 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
         }
 
     Vec3f reflect_dir = reflect(dir, N).normalize();
+    Vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
      Vec3f reflect_orig;
+    Vec3f refract_orig;
     
-    if (reflect_dir*N <0) // offset the original point to avoid occlusion by the object itself
+     if (reflect_dir*N <0) // offset the original point to avoid occlusion by the object itself
     {
         reflect_orig = point - N*1e-3;
     }
     else
     {
         reflect_orig = point + N*1e-3;
-    } 
-    
+    }  
+
+      if (refract_dir*N < 0) // offset the original point to avoid occlusion by the object itself
+    {
+        refract_orig = point - N*1e-3;
+    }
+    else
+    {
+        refract_orig = point + N*1e-3;
+    }  
     Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
+
+
     
     
     float diffuse_light_intensity = 0, specular_light_intensity = 0;
     for (size_t i=0; i<lights.size(); i++) {
         Vec3f light_dir      = (lights[i].position - point).normalize();
         float light_distance = (lights[i].position - point).norm();
-        
-        
-         Vec3f shadow_orig;
+
+        Vec3f shadow_orig;
         if (light_dir*N < 0) // checking if the point lies in the shadow of the lights[i]
         {
           shadow_orig = point - N*1e-3;
@@ -130,7 +156,7 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
         diffuse_light_intensity  += lights[i].intensity * std::max(0.f, light_dir*N);
         specular_light_intensity += powf(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular)*lights[i].intensity;
     }
-    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1] + reflect_color*material.albedo[2];
+    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1] + reflect_color*material.albedo[2] + refract_color*material.albedo[3];
     //return material.diffuse_color;
 }
 
@@ -165,9 +191,10 @@ int main(int argc, const char** argv)
     sceneId = atoi(cmdLineParams["-scene"].c_str());
 
 
-    Material      ivory(Vec3f(0.6,  0.3, 0.1), Vec3f(0.4, 0.4, 0.3),   50.);
-    Material red_rubber(Vec3f(0.9,  0.1, 0.0), Vec3f(0.3, 0.1, 0.1),   10.);
-    Material     mirror(Vec3f(0.0, 10.0, 0.8), Vec3f(1.0, 1.0, 1.0), 1425.);
+    Material      ivory( Vec4f(0.6,  0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3),   50. , 1.0);
+    Material      glass( Vec4f(0.0,  0.5, 0.1, 0.8), Vec3f(0.6, 0.7, 0.8),  125. , 1.5);
+    Material red_rubber( Vec4f(0.9,  0.1, 0.0, 0.0), Vec3f(0.3, 0.1, 0.1),   10. , 1.0);
+    Material     mirror( Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425. , 1.0);
 
     // Material      ivory(0x00CDE5FC);
     // Material red_rubber(0x000E1A74);
@@ -178,7 +205,7 @@ int main(int argc, const char** argv)
    if(sceneId == 1)
     {
       spheres.push_back(Sphere(Vec3f(-3,    0,   -16), 2,      ivory));
-      spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2,      mirror));
+      spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2,      glass));
       spheres.push_back(Sphere(Vec3f( 1.5, -0.5, -18), 3, red_rubber));
       spheres.push_back(Sphere(Vec3f( 7,    5,   -18), 4,     mirror));
 
